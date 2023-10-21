dbb30b02fde26678a22e5c5149902578c3153854
abhishekkjainn <jainabhishek1904@gmail.com>
2023-10-21 18:58:59 +0530

    Added the guide page

--- script.js
+++ script.js
@@ -11,3 +11,31 @@ function check() {
     headercount++;
   }
 }
+
+let guidecount = 0;
+function guide() {
+  let stringlist = [
+    'Seamless Auto Bookings Navigate Vellore with Ease',
+    'Trustworthy and Verified Drivers: Your Journey, Your Confidence!',
+    'Automate Vellore: Affordable Rides, Unbeatable Prices!',
+    'Automate Vellore: Where Trust Drives Every Journey.',
+  ];
+  let colorlist = ['#5D3FD3', '#FFE948', '#3CDC56', '#FFE948'];
+  let imagelist = [
+    'assets/headerimage.png',
+    'assets/guide2.jpg',
+    'assets/guide3.jpg',
+    'assets/guide4.jpg',
+  ];
+  let guidepage = document.getElementById('guide');
+  let guidetext = document.getElementById('guidetext');
+  let guideimage = document.getElementById('guideimage');
+  if (guidecount <= 3) {
+    guidetext.innerText = stringlist[guidecount];
+    guidepage.style.backgroundColor = colorlist[guidecount];
+    guideimage.src = imagelist[guidecount];
+    guidecount++;
+  } else {
+    guidepage.style.display = 'none';
+  }
+}
